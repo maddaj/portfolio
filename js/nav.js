cca445d2e9d5dd71aa5dafaf46e5cf2bd6b5b8e8
maddaj <j.maddalena2@gmail.com>
2020-06-11 14:06:43 +0200

    Part project

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,45 @@
 $(window).scroll(function () {
     if ($(document).scrollTop() > 700) {
         $('.nav-icon').addClass('nav-icon-display');
-        $('.nav-download').addClass('nav-download-display');
     } else {
         $('.nav-icon').removeClass('nav-icon-display');
-        $('.nav-download').addClass('nav-download-display');
     }
 });
+
+const sections = [...document.querySelectorAll('section')];
+const link = (id) => document.querySelector('a[href="#${ id }"]');
+
+const inView = (element) => {
+    let top = element.offsetTop;
+    let height = element.offsetHeight;
+
+    while (element.offsetParent) {
+        element = element.offsetParent;
+        top = element.offsetTop;
+    }
+
+    return (
+        top < (window.pageYOffset + window.innerHeight) && (top + height) > window.pageYOffset
+    );
+}
+
+const init = () => {
+    function update() {
+        let next = false;
+
+        sections.forEach(section => {
+            const current = link(sectin.id);
+
+            if (inView(section) && !next) {
+                current.classList.add('current');
+                next = true;
+            } else {
+                current.classList.remove('current');
+            }
+        });
+    }
+    update();
+    window.addEventListener('scroll', update);
+}
+
+init();
