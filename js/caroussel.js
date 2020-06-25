5190a6fc3a653cff4daf28e302d7590588971142
maddaj <j.maddalena2@gmail.com>
2020-06-25 12:40:15 +0200

    Add wheeler-landing

--- js/caroussel.js
+++ js/caroussel.js
@@ -1,6 +1,43 @@
 // https://kenwheeler.github.io/slick/
 
 $(document).ready(function () {
+    $('.caroussel-wheeler').slick({
+        dots: false,
+        infinite: false,
+        speed: 2000,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        prevArrow: false,
+        nextArrow: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: false,
+                    dots: false,
+                    prevArrow: false,
+                    nextArrow: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
     $('.caroussel').slick({
         dots: false,
         infinite: true,
@@ -12,41 +49,15 @@ $(document).ready(function () {
         prevArrow: false,
         nextArrow: false
     });
+    $('.caroussel-cpt').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        speed: 2000,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: false,
+        nextArrow: false
+    });
+
 });
-$('.caroussel-wheeler').slick({
-    dots: false,
-    infinite: false,
-    speed: 2000,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    prevArrow: false,
-    nextArrow: false,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: false,
-                dots: false,
-                prevArrow: false,
-                nextArrow: false
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-})
