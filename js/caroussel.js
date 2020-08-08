4c3fa434cc672f09b2268b8e571678a0f2efbbe1
maddaj <j.maddalena2@gmail.com>
2020-08-08 12:42:49 +0200

    Add svisory project

--- js/caroussel.js
+++ js/caroussel.js
@@ -59,5 +59,33 @@ $(document).ready(function () {
         prevArrow: false,
         nextArrow: false
     });
-
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 1,
+        prevArrow: false,
+        nextArrow: false,
+        infinite: true,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
